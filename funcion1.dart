51305f58d05971cb988dd748caa978b29b288ee8
danielaj22 <a.22308051281069@cbtis128.edu.mx>
2025-02-07 16:48:17 +0000

    mi primera vez

---
+++ funcion1.dart
@@ -0,0 +1,16 @@
+// funcion que no devuelve valor 
+void saludar(){
+  print('hola mundo');
+}
+void escribemensaje(){
+  print('daniela jurado');
+}
+void main(){
+  // daniela jurado mat:22308051281069
+  //llamamos a la funcion saludar
+  print('daniela jurado mat:22308051281069');
+  print('llamado a la funcion saludar');
+  saludar();
+  print('llamado a la funcion escribemensaje');
+  escribemensaje();
+}
